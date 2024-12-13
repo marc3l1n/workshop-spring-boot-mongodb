7fccd6e3807d76d7fbd2ec2bd726ca79651b0f96
Marcelo Oliveira <marcelorodrigooliveira@gmail.com>
2024-12-12 22:46:02 -0300

    Insercao de usuario com POST

--- src/main/java/com/marcelo/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/marcelo/workshopmongo/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.marcelo.workshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-	
+public interface UserRepository extends MongoRepository<User, String> {	
 }
